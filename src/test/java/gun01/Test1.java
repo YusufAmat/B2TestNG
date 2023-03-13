644ff32aa49c47005f1068d624daa7a56f3b7637
atezgider <atezgider@gmail.com>
2023-03-13 20:39:40 +0100

    2023-03-13

--- src/test/java/gun01/Test1.java
+++ src/test/java/gun01/Test1.java
@@ -1,14 +1,49 @@
 package gun01;
 
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
 public class Test1 {
 
+    String url = "https://demowebshop.tricentis.com/";
+    String myUsername = "abc123@aa.com";
+    String myPassword = "123456";
+    By lLoginformUsername = By.id("Email");
+    By lLoginformPassword = By.id("Password");
+    By lLoginformSubmitButton = By.xpath("//input[@value='Log in']");
+    By lLogout = By.xpath("//a[text()='Log out']");
+    By lLogin = By.xpath("//a[text()='Log in']");
+
+
     @Test
-    public void test1(){
-        int num = 1;
-        Assert.assertEquals(num,1);
+    public void gotoUrl(){
+        // driver.get
+        // title assert edilecek
     }
+
+    @Test
+    public void clickLinkLogin(){
+        // log in linkine tiklanacak
+    }
+
+    @Test
+    public void fulfillloginForm(){
+        // loginform dolduruacak
+    }
+
+    @Test
+    public void loginSubmitClick(){
+        // loginform click butonuna tiklanacak
+    }
+
+    @Test
+    public void loginAssert(){
+        // login olundugu assert edilecek
+    }
+
+
+
+
 }
