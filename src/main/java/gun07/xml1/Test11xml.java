5b54fb408ec8586c8c16b65b252828be49d2a706
atezgider <atezgider@gmail.com>
2023-03-21 20:30:34 +0100

    2023-03-21

---
+++ src/main/java/gun07/xml1/Test11xml.java
@@ -0,0 +1,28 @@
+package gun07.xml1;
+
+import org.testng.annotations.Test;
+
+public class Test11xml {
+
+
+    @Test
+    public void test1(){
+        System.out.println("Test5");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Test6");
+    }
+
+    @Test
+    public void test3(){
+        System.out.println("Test7");
+    }
+
+    @Test
+    public void test4(){
+        System.out.println("Test8");
+    }
+
+}
